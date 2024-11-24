e2ffd52f4ad3e19e43be8205503aec709db6062d
Rafael Ferreira Fernandes <rafael680ferreira@gmail.com>
2024-11-24 15:05:30 -0300

    Add files via upload

---
+++ chatbot-loja-carro-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatbot-loja-carro-bot-f93b-Web-Deploy.pubxml -p:Password=WopzE5B5FZ3iN6EtwHjhTRwz1rXqEcjKiR5BqPg1ntSsGzMduWsRsSRsermp
+
